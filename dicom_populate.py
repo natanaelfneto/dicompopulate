423ca70de002d7efe626585de04bc88b09380446
natanaelfneto <natanaelfneto@outlook.com>
2018-09-05 13:08:22 -0300

    initial commit

---
+++ dicom_populate.py
@@ -0,0 +1,15 @@
+import os
+
+files = []
+i = 0
+
+for root, dirs, files in os.walk('D:/PACS/'):
+    if files:
+        for file in files:
+            file_path = os.path.abspath(os.path.join(root, file))
+            print("Sending "+file_path)
+            command = "dcmsnd DCM4CHEE@localhost:11112 "+file_path
+            os.system(command)
+            break
+
+print('End')
