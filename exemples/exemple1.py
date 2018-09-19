438adb86df83bc71b92614469f2379df9154be18
natanaelfneto <natanaelfneto@outlook.com>
2018-09-19 17:15:56 -0300

    fix typo in file

--- exemples/exemple1.py
+++ exemples/exemple1.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-from __future__ import print_function
-from __future__ import absolute_import
-
 from populate import populate
 
 # get all files and paths to send
@@ -9,7 +5,7 @@ path_1 = '/PACS/1/'
 path_2 = '/PACS/2/'
 path_3 = '/PACS/3/dicom.dcm'
 
-# get all desired conections to receive
+# get all desired connections to receive
 c_1 = 'DCM4CHEE@10.0.0.1:11112'
 c_2 = 'OTHER@127.0.0.1:5555'
 
@@ -19,5 +15,5 @@ populate.run(
   verbose=True,
   max_workers=10,
   paths=[ path_1, path_2, path_3 ],
-  conections=[ c_1, c_2 ]
+  connections=[ c_1, c_2 ]
 )
